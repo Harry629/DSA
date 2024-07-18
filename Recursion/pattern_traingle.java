0f900fb915d6f548cefe98affa3a2cb21507bbce
HARI OM <89219401+Harry629@users.noreply.github.com>
2024-07-18 19:38:36 +0530

    Update pattern_traingle.java

--- Recursion/pattern_traingle.java
+++ Recursion/pattern_traingle.java
@@ -1,7 +1,7 @@
-// ****
-// ***
-// **
-// *
+// * * * *  |   *
+// * * *    |   * *
+// * *      |   * * *
+// *        |   * * * *
 
 class HelloWorld {
     public static void main(String[] args) {
@@ -17,6 +17,18 @@ class HelloWorld {
         }else{
             System.out.println();
             triangle(r-1,0);
+        }
+    }
+    public static void triangle2(int r,int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            triangle2(r,c+1);
+            System.out.print("*");
+        }else{
+            triangle2(r-1,0);
+            System.out.println();
         }
     }
 }
