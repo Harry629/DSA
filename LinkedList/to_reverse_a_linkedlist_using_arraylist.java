8529f2456ffdbc74d6ed3f51905ddb04782c2f31
HARI OM <89219401+Harry629@users.noreply.github.com>
2024-07-22 11:10:57 +0530

    Update to_reverse_a_linkedlist_using_arraylist.java

--- LinkedList/to_reverse_a_linkedlist_using_arraylist.java
+++ LinkedList/to_reverse_a_linkedlist_using_arraylist.java
@@ -1,15 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.*;
 class Solution {
-    public ListNode reverseList(ListNode head) {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public static ListNode reverseList(ListNode head) {
         ArrayList<Integer> arr = new ArrayList<>();
         ListNode temp = head;
         while(temp != null){
@@ -24,3 +22,18 @@ class Solution {
         }
         return temp2.next;
     }
+    public static void main(String[] args){
+        ListNode ll = new ListNode(1);
+        ll.next= new ListNode(2);
+        ll.next.next = new ListNode(3);
+        ll.next.next.next = new ListNode(4);
+        ll.next.next.next.next = new ListNode(5);
+        ll.next.next.next.next.next = new ListNode(6);
+        ListNode ll1 = reverseList(ll);
+        while(ll1!= null){
+            System.out.println(ll1.val);
+            ll1= ll1.next;
+        }
+    }
+    
+}
