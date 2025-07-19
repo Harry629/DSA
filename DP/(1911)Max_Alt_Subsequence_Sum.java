c35c9fa9afdb9346244bd8b79902ce4392aa372b
HARI OM <89219401+Harry629@users.noreply.github.com>
2025-07-19 19:18:24 +0530

    Update (1911)Max_Alt_Subsequence_Sum.java

--- DP/(1911)Max_Alt_Subsequence_Sum.java
+++ DP/(1911)Max_Alt_Subsequence_Sum.java
@@ -25,3 +25,20 @@ class Solution {
         return dp[idx][f]=Math.max(skip, take);
     }
 }
+
+
+--------------------------- BottomUp approach ------------------------------------
+
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        int n = nums.length;
+        long[][] dp = new long[nums.length+1][2];
+        for(int i =1;i<n+1;i++){
+            //even
+            dp[i][0]= Math.max(dp[i-1][1]-nums[i-1],dp[i-1][0]);
+            //odd
+            dp[i][1] = Math.max(dp[i-1][0]+nums[i-1],dp[i-1][1]); 
+        }
+        return Math.max(dp[n][0],dp[n][1]);
+    }
+}
