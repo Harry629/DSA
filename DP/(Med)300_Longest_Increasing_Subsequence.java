66f9fd85f3e8cef626e9ee4745bc7fa371b333dd
HARI OM <89219401+Harry629@users.noreply.github.com>
2025-08-13 09:43:44 +0530

    Update (Med)300_Longest_Increasing_Subsequence.java

--- DP/(Med)300_Longest_Increasing_Subsequence.java
+++ DP/(Med)300_Longest_Increasing_Subsequence.java
@@ -32,3 +32,22 @@ Explanation: The longest increasing subsequence is [2,3,7,101], therefore the le
         return Math.max(take, skip);
     }
 }
+
+------------------- Using Bottom-Up Approach -------------------------------
+
+  class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] t = new int[nums.length];
+        Arrays.fill(t,1);
+        int max = 1;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    t[i]= Math.max(t[i],t[j]+1);
+                    max = Math.max(max,t[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
