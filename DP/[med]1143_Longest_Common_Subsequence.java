0dae80527e9ba4c059feaac91e8d6a5f8e6de821
HARI OM <89219401+Harry629@users.noreply.github.com>
2026-08-09 15:44:35 +0530

    Add tabulation approach for LCS problem
    Implemented tabulation method for longest common subsequence.

--- DP/[med]1143_Longest_Common_Subsequence.java
+++ DP/[med]1143_Longest_Common_Subsequence.java
@@ -35,3 +35,33 @@ class Solution {
         return dp[i][j]=Math.max(take,(Math.max(c1, c2)));
     }
 }
+
+---------------------- Using tabulation ------------------------
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][m]= 0;
+        }
+        for(int j=0;j<=m;j++){
+            dp[n][j]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(text1.charAt(i)==text2.charAt(j)){
+                    dp[i][j]= 1+ dp[i+1][j+1];
+                }else{
+                    dp[i][j]= Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
+
+    }
+}
